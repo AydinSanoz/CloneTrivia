7ed03a0ff3198d420ed9b5450810e57efb8bcdaf
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-23 12:01:02 +0300

    intro page and animations completed

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { reducer, initialStates } from './context';
+import {createStore} from 'redux'
 
-import {Intro, Questions, Finish} from './pages';
+import { Intro, Questions, Finish } from './pages';
 
 const Stack = createStackNavigator();
+const store = createStore(reducer, initialStates);
 
 function Router() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Intro" component={Intro} />
-        <Stack.Screen name="Finish" component={Finish} />
-        <Stack.Screen name="Questions" component={Questions} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<Stack.Navigator screenOptions={{ headerShown: false }}>
+					<Stack.Screen name='Intro' component={Intro} />
+					{/* <Stack.Screen name='Finish' component={Finish} />
+					<Stack.Screen name='Questions' component={Questions} /> */}
+				</Stack.Navigator>
+			</NavigationContainer>
+		</Provider>
+	);
 }
 
 export default Router;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './QuestionItem';
 export * from './CategorySelectModal';
+export * from './TimerComponent';
